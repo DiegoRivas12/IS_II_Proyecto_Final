a89c7707b52459a44a5dba3bdbbfdb832533bf78
Diego Rivas <drivash@unsa.edu.pe>
2022-12-27 12:16:05 -0500

    Actualizacion correcta del proyecto

--- src/component/editor.js
+++ src/component/editor.js
@@ -81,7 +81,7 @@ function inputEventHandler(evt) {
       resetTextareaSize.call(this);
       this.change('input', v);
     } else {
-      evt.target.value = cell.text;
+      evt.target.value = cell.text || '';
     }
   } else {
     this.inputText = v;
@@ -249,6 +249,8 @@ export default class Editor {
   }
 
   setCell(cell, validator) {
+    if (cell && cell.editable === false) return;
+
     // console.log('::', validator);
     const { el, datepicker, suggest } = this;
     el.show();
--- src/component/sheet.js
+++ src/component/sheet.js
@@ -332,7 +332,15 @@ function cut() {
 function paste(what, evt) {
   const { data } = this;
   if (data.settings.mode === 'read') return;
-  if (data.paste(what, msg => xtoast('Tip', msg))) {
+  if (data.clipboard.isClear()) {
+    const resetSheet = () => sheetReset.call(this);
+    const eventTrigger = (rows) => {
+      this.trigger('pasted-clipboard', rows);
+    };
+    // pastFromSystemClipboard is async operation, need to tell it how to reset sheet and trigger event after it finishes
+    // pasting content from system clipboard
+    data.pasteFromSystemClipboard(resetSheet, eventTrigger);
+  } else if (data.paste(what, msg => xtoast('Tip', msg))) {
     sheetReset.call(this);
   } else if (evt) {
     const cdata = evt.clipboardData.getData('text/plain');
@@ -469,7 +477,15 @@ function horizontalScrollbarMove(distance) {
 function rowResizerFinished(cRect, distance) {
   const { ri } = cRect;
   const { table, selector, data } = this;
-  data.rows.setHeight(ri, distance);
+  const { sri, eri } = selector.range;
+  if (ri >= sri && ri <= eri) {
+    for (let row = sri; row <= eri; row += 1) {
+      data.rows.setHeight(row, distance);
+    }
+  } else {
+    data.rows.setHeight(ri, distance);
+  }
+
   table.render();
   selector.resetAreaOffset();
   verticalScrollbarSet.call(this);
@@ -479,8 +495,15 @@ function rowResizerFinished(cRect, distance) {
 function colResizerFinished(cRect, distance) {
   const { ci } = cRect;
   const { table, selector, data } = this;
-  data.cols.setWidth(ci, distance);
-  // console.log('data:', data);
+  const { sci, eci } = selector.range;
+  if (ci >= sci && ci <= eci) {
+    for (let col = sci; col <= eci; col += 1) {
+      data.cols.setWidth(col, distance);
+    }
+  } else {
+    data.cols.setWidth(ci, distance);
+  }
+
   table.render();
   selector.resetAreaOffset();
   horizontalScrollbarSet.call(this);
