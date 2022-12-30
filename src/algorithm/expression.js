d5ed8583437f3d051781ea38167f7b4c620a4fec
Diego Rivas <drivash@unsa.edu.pe>
2022-12-29 19:03:40 -0500

    reduce its Cognitive Complexity expresion.js

--- src/algorithm/expression.js
+++ src/algorithm/expression.js
@@ -5,27 +5,17 @@ const infix2suffix = (src) => {
   const stack = [];
   for (let i = 0; i < src.length; i += 1) {
     const c = src.charAt(i);
-    if (c !== ' ') {
-      if (c >= '0' && c <= '9') {
-        stack.push(c);
-      } else if (c === ')') {
-        let c1 = operatorStack.pop();
-        while (c1 !== '(') {
-          stack.push(c1);
-          c1 = operatorStack.pop();
-        }
-      } else {
-        // priority: */ > +-
-        if (operatorStack.length > 0 && (c === '+' || c === '-')) {
-          const last = operatorStack[operatorStack.length - 1];
-          if (last === '*' || last === '/') {
-            while (operatorStack.length > 0) {
-              stack.push(operatorStack.pop());
-            }
-          }
-        }
-        operatorStack.push(c);
+    if (c !== ' '&& c >= '0' && c <= '9')stack.push(c); 
+    else if (c === ')') {
+      let c1 = operatorStack.pop();
+      while (c1 !== '(') {
+        stack.push(c1);
+        c1 = operatorStack.pop();
       }
+    } else if (operatorStack.length > 0 && (c === '+' || c === '-')) {
+        const last = operatorStack[operatorStack.length - 1];
+        if (last === '*' || last === '/') stack.push(operatorStack.pop());
+      operatorStack.push(c);
     }
   }
   while (operatorStack.length > 0) {
